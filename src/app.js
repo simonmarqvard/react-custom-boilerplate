b583ae8ca972af29c1a11947add0da5fd03ea825
Simon Jensen <Simonmarqvard@gmail.com>
2019-08-04 22:12:52 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.css";
+
+const jsx = <p> My name is simon </p>;
+
+console.log("my name is Simon bob Marqvard Jensen");
+
+ReactDOM.render(jsx, document.getElementById("app"));
